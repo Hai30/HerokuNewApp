74c26703de2e336e02e15ff94d516cee6d61f2ba
Giovanna Miranda <hai1361988@gmail.com>
2021-08-05 01:42:18 +0300

    Procfile

--- server/server.js
+++ server/server.js
@@ -1,12 +1,21 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3002;
-app.use(express.static(publicPath));
+const publicPath = path.join(__dirname, '..', './public');
+// const port = process.env.PORT || 3002;
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+ 
+ app.use(express.static(publicPath));
 app.get('*', (req, res) => {
    res.sendFile(path.join(publicPath, 'index.html'));
-});
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+ });
+// app.listen(port, () => {
+//    console.log('Server is up!');
+// });
+
+app.listen(port, function() {
+  console.log("Server started succesfully");
+});            
